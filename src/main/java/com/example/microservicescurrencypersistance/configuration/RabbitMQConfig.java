cab960e2056c828232740c076f4b985aea6f194d
kondarbak <kondarbak@gmaill.com>
2024-04-15 09:32:14 +0200

    Maintenance updates

--- src/main/java/com/example/microservicescurrencypersistance/configuration/RabbitMQConfig.java
+++ src/main/java/com/example/microservicescurrencypersistance/configuration/RabbitMQConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
